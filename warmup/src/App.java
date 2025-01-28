35d46d27ebea32f0563574986a8e90865ab82068
dishb <code.dishb@gmail.com>
2025-01-28 09:00:27 -0800

    fix: add correct solution for column major traversal

--- warmup/src/App.java
+++ warmup/src/App.java
@@ -2,11 +2,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         int[][] arr = new int[4][3];
         int current = 0;
-        for (int[] row : arr) {
-            for (int col = 0; col < row.length; col++) {
-                row[col] = current + (arr.length * col);
+
+        // for (int[] row : arr) {
+        //     for (int col = 0; col < row.length; col++) {
+        //         row[col] = current + (arr.length * col);
+        //     }
+        //     current++;
+        // }
+
+        for (int col = 0; col < arr[0].length; col++) {
+            for (int row = 0; row < arr.length; row++) {
+                arr[row][col] = current;
+                current++;
             }
-            current++;
         }
 
         for (int[] row : arr) {
