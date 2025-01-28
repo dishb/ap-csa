d46c65e686515509e5735c632c9bfdd5fb5dbcb8
dishb <code.dishb@gmail.com>
2025-01-28 08:42:17 -0800

    chore: add 1/28/25 warmup

--- warmup/src/App.java
+++ warmup/src/App.java
@@ -1,38 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
-        int a = -2;
-        int b = 5;
-        int c = 11;
-
-        multiples12(a);
-        multiples12(b);
-        multiples12(c);
-        
-        int f = ifMethod(a, b);
-        int g = ifMethod(b, c);
-        int h = ifMethod(c, a);
-        
-        System.out.println(f);
-        System.out.println(g);
-        System.out.println(h);
-
-    }
-    public static void multiples12(int d){
-        for(int i = 1; i < 13; i++){
-            int e = d * i;
-            System.out.println(e);
-        }
-    }
-
-    public static int ifMethod(int j, int k) {
-        if (j > k) {
-            return (j ^ 2 + k);
-        } else if (j < k) {
-            return (k - j);
+        int[][] arr = new int[4][3];
+        int current = 0;
+        for (int[] row : arr) {
+            for (int col = 0; col < row.length; col++) {
+                row[col] = current + (4 * col);
+            }
+            current++;
         }
 
-        return -1;
+        for (int[] row : arr) {
+            for (int col : row) {
+                System.out.print(col + " ");
+            }
+            System.out.println("");
+        }
     }
-
 }
