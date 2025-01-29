b6009222b0c181d00e938da050425b3a1f9ceafe
dishb <code.dishb@gmail.com>
2025-01-29 13:19:25 -0800

    chore: format code

--- concentration/src/Board.java
+++ concentration/src/Board.java
@@ -1,164 +1,91 @@
-/** 
- * A game board of NxM board of tiles.
- * 
- *  @author PLTW
- *  @version 2.0
- */
-
 import java.util.ArrayList;
 
-/** 
- * A Board class for concentration
- */
+public class Board {  
+    private static String[] tileValues = {"lion", "lion",
+                                          "penguin", "penguin",
+                                          "dolphin", "dolphin",
+                                          "fox", "fox",
+                                          "monkey", "monkey",
+                                          "turtle", "turtle"
+                                          }; 
+    private ArrayList<Tile> tileValuesList = new ArrayList<Tile>();
+    private Tile[][] gameboard = new Tile[3][4];
 
-public class Board
-{  
-  private static String[] tileValues = {"lion", "lion",
-                                        "penguin", "penguin",
-                                        "dolphin", "dolphin",
-                                        "fox", "fox",
-                                        "monkey", "monkey",
-                                        "turtle", "turtle"
-                                        }; 
-  private ArrayList<Tile> tileValuesList = new ArrayList<Tile>();
-  private Tile[][] gameboard = new Tile[3][4];
-
-  /**  
-   * Constructor for the game. Creates the 2D gameboard
-   * by populating it with card values
-   * 
-   */
-  public Board()
-  {
-    for (String value : tileValues) {
-        tileValuesList.add(new Tile(value));
-    }
-
-    for (int i = 0; i < gameboard.length; i++) {
-        for (int j = 0; j < gameboard[0].length; j++) {
-            int randomTile = (int) (Math.random() * tileValuesList.size());
-            gameboard[i][j] = new Tile(tileValuesList.remove(randomTile).getValue());
+    public Board() {
+        for (String value : tileValues) {
+            tileValuesList.add(new Tile(value));
         }
-    }
-  }
 
- /** 
-   * Returns a string representation of the board, getting the state of
-   * each tile. If the tile is showing, displays its value, 
-   * otherwise displays it as hidden.
-   * 
-   * Precondition: gameboard is populated with tiles
-   * 
-   * @return a string represetation of the board
-   */
-  public String toString()
-  {
-    String boardString = "";
-
-    for (Tile[] row : gameboard) {
-        for (Tile tile : row) {
-            if (tile.isShowingValue()) {
-                boardString += tile.getValue() + " ";
-            } else {
-                boardString += tile.getHidden() + " ";
-            }
-        }
-        boardString += "\n";
-    }
- 
-    return boardString;
-  }
-
-  /** 
-   * Determines if the board is full of tiles that have all been matched,
-   * indicating the game is over.
-   * 
-   * Precondition: gameboard is populated with tiles
-   * 
-   * @return true if all tiles have been matched, false otherwse
-   */
-  public boolean allTilesMatch()
-  {
-    for (Tile[] row : gameboard) {
-        for (Tile tile : row) {
-            if (tile.matched() == false) {
-                return false;
+        for (int i = 0; i < gameboard.length; i++) {
+            for (int j = 0; j < gameboard[0].length; j++) {
+                int randomTile = (int) (Math.random() * tileValuesList.size());
+                gameboard[i][j] = new Tile(tileValuesList.remove(randomTile).getValue());
             }
         }
     }
 
-    return true;
-  }
+    public String toString() {
+        String boardString = "";
 
-  /** 
-   * Sets the tile to show its value (like a playing card face up)
-   * 
-   * Preconditions:
-   *   gameboard is populated with tiles,
-   *   row values must be in the range of 0 to gameboard.length,
-   *   column values must be in the range of 0 to gameboard[0].length
-   * 
-   * @param row the row value of Tile
-   * @param column the column value of Tile
-   */
-  public void showValue (int row, int column)
-  {
-    gameboard[row][column].show();
-  }  
+        for (Tile[] row : gameboard) {
+            for (Tile tile : row) {
+                if (tile.isShowingValue()) {
+                    boardString += tile.getValue() + " ";
+                } else {
+                    boardString += tile.getHidden() + " ";
+                }
+            }
+            boardString += "\n";
+        }
 
-  /** 
-   * Checks if the Tiles in the two locations match.
-   * 
-   * If Tiles match, show Tiles in matched state and return a "matched" message
-   * If Tiles do not match, re-hide Tiles (turn face down).
-   * 
-   * Preconditions:
-   *   gameboard is populated with Tiles,
-   *   row values must be in the range of 0 to gameboard.length,
-   *   column values must be in the range of 0 to gameboard[0].length
-   * 
-   * @param row1 the row value of Tile 1
-   * @param col1 the column value of Tile 1
-   * @param row2 the row value of Tile 2
-   * @param col2 the column vlue of Tile 2
-   * @return a message indicating whether or not a match occured
-   */
-  public String checkForMatch(int row1, int col1, int row2, int col2)
-  {
-    String msg = "No match found.";
-
-     if (gameboard[row1][col1].getValue().equals(gameboard[row2][col2].getValue())) {
-        gameboard[row1][col1].foundMatch();
-        gameboard[row2][col2].foundMatch();
-        msg = "Match found.";
-     } else {
-        gameboard[row1][col1].hide();
-        gameboard[row2][col2].hide();
-     }
-    
-     return msg;
-  }
-
-  /** 
-   * Checks the provided values fall within the range of the gameboard's dimension
-   * and that the tile has not been previously matched
-   * 
-   * @param row the row value of Tile
-   * @param col the column value of Tile
-   * @return true if row and col fall on the board and the row,col tile is unmatched, false otherwise
-   */
-  public boolean validateSelection(int row, int col)
-  {
-
-    if (row < 0 || row >= gameboard.length || col < 0 || col >= gameboard[0].length) {
-        return false;
+        return boardString;
     }
 
-    if (gameboard[row][col].matched()) {
-        return false;
+    public boolean allTilesMatch() {
+        for (Tile[] row : gameboard) {
+            for (Tile tile : row) {
+                if (tile.matched() == false) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
-    return true;
-  }
+    public void showValue (int row, int column) {
+        gameboard[row][column].show();
+    }  
 
+    public String checkForMatch(int row1, int col1, int row2, int col2) {
+        String msg = "No match found.";
+
+        if (gameboard[row1][col1].getValue().equals(gameboard[row2][col2].getValue())) {
+            gameboard[row1][col1].foundMatch();
+            gameboard[row2][col2].foundMatch();
+            msg = "Match found.";
+        } else {
+            gameboard[row1][col1].hide();
+            gameboard[row2][col2].hide();
+        }
+
+        if (msg.equals("No match found.")) {
+            AudioPlayer.playSound("/Users/dishb/Documents/ap-csa/concentration/sounds/error.wav");
+        }
+
+        return msg;
+    }
+
+    public boolean validateSelection(int row, int col) {
+
+        if (row < 0 || row >= gameboard.length || col < 0 || col >= gameboard[0].length) {
+            return false;
+        }
+
+        if (gameboard[row][col].matched()) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- concentration/src/Tile.java
+++ concentration/src/Tile.java
@@ -1,131 +1,58 @@
-/**
- * A tile in the game of Concentration
- * 
- * Tiles are arranged on an n x m board, represented in a 2D array.
- * 
- * A tile is similar to a playing card and tiles are 
- * shown in 1 of 3 states:
- *   with their value (face up)
- *   as hidden (face down)
- *   as matched (no long in play)
- * 
- * @author PLTW
- * @version 2.0
- */
+public class Tile {   
+    private boolean isMatched;
+    private boolean isShowing;
+    private String value;
+    private String hidden;
+    private String matched;
 
-/**
- * A Tile class for a gameboard
- */
-public class Tile
-{   
-  private boolean isMatched;
-  private boolean isShowing;
-  
-  private String value;
-  private String hidden;
-  private String matched;
-  
-  /**
-   * Construct a tile with a string value. The default state
-   * of a tile is hidden (face down) on the board and is unmatched.
-   * 
-   * @param  value the string value of the tile (face up value)
-   */
-  public Tile(String value) 
-  {
-    this.value = value; // face up value
+    public Tile(String value) {
+        this.value = value;
 
-    isMatched = false;
-    isShowing = false;
-    hidden =  "_____"; //  face down value
-    matched = "  *  ";   // a tile that is no longer in play
-  }
+        isMatched = false;
+        isShowing = false;
+        hidden =  "_____";
+        matched = "  *  ";
+    }
 
-  /**
-   * Get the value of this tile (face up value)
-   *  
-   * @return the value of the tile
-   */
-  public String getValue()
-  {
-    return value;
-  }
-    
-  /**
-   * Get the hidden value of this tile (face down value)
-   * 
-   * @return the hidden string
-   */
-  public String getHidden()
-  {
-    return hidden;
-  }
+    public String getValue() {
+        return value;
+    }
 
-  /**
-   * Show this tile (turn it face up)
-   */
-  public void show()
-  {
-    isShowing = true;
-  }
+    public String getHidden() {
+        return hidden;
+    }
 
-  /** 
-   * Hide this tile (turn it face down)
-   */
-  public void hide()
-  {
-    isShowing = false;
-  }
+    public void show() {
+        isShowing = true;
+    }
 
-  /**
-   * Determine if the tile is currently showing its value (face up)
-   * 
-   * @return true if the tile is showing a value, false otherwise
-   */
-  public boolean isShowingValue()
-  {
-    return isShowing;
-  }
+    public void hide() {
+        isShowing = false;
+    }
 
-  /**
-   * A matching tile to this tile has been found, set matched to true
-   * and change the way the tile is shown 
-   */
-  public void foundMatch()
-  {
-    isMatched = true;
-    value = matched;
-    hidden = matched;
-  }
-  
-  /**
-   * Determine if this tile has been previously matched
-   * 
-   * @return true of the tile was previously matched, false otherwise
-   */
-  public boolean matched()
-  {
-    return isMatched;
-  }
-    
-  /**
-   * Determines if two tiles have the same value
-   * 
-   * @param tile the tile to compare this tile to
-   * @return true if their values are the same, false otherwise
-   */
-  public boolean equals(Tile tile)
-  {      
-    if (this.value.equals(tile.value))
-      return true;
-    return false;
-  }
+    public boolean isShowingValue() {
+        return isShowing;
+    }
 
-  /**
-   * Return the state of this tile
-   */
-  public String toString()
-  {
-    return value;
-  }
+    public void foundMatch() {
+        isMatched = true;
+        value = matched;
+        hidden = matched;
+    }
+
+    public boolean matched() {
+        return isMatched;
+    }
+
+    public boolean equals(Tile tile) {      
+        if (this.value.equals(tile.value)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public String toString() {
+        return value;
+    }
 }
